9133f71c6e11644528ba1b6b07393d6328f7566d
alokverma <alokverma749@gmail.com>
2025-01-18 20:42:13 +0530

    temp: prod webhook test

--- app/api/payment/webhook/route.ts
+++ app/api/payment/webhook/route.ts
@@ -5,11 +5,14 @@ import { validateWebhookSignature } from "razorpay/dist/utils/razorpay-utils";
 
 export async function POST(req: NextRequest) {
   try {
-    const webhookSignature = req.headers.get("x-razorpay-signature") || "";
+    const webhookSignature = req.headers.get("X-Razorpay-Signature") || "";
     const body = await req.json();
-    const isValid = await validateWebhookSignature(webhookSignature, body, process.env.RAZORPAY_WEBHOOK_SECRET || "");
+
+    // why isValid is false?
+    const isValid = await validateWebhookSignature(webhookSignature, body, process.env.RAZORPAY_WEBHOOK_SECRET as string);
 
     console.log(
+      req,
       body,
       body.payload.payment.entity,
       webhookSignature,
@@ -22,27 +25,20 @@ export async function POST(req: NextRequest) {
     }
 
     const paymentDetails = body.payload.payment.entity;
-    console.log(paymentDetails, '###paymentDetails');
 
     const payment = await Payment.findOne({ orderId: paymentDetails.order_id });
-    console.log(payment, '###payment');
-
     payment.status = paymentDetails.status;
     await payment.save();
-    console.log(payment, '###payment');
-    console.log(body.event, '###body.event');
 
     if (body.event === "payment.captured") {
       const order = await Order.findOne({ razorpayOrderId: paymentDetails.order_id });
-      console.log(order, '###order');
       order.paymentStatus = "captured";
-      console.log(order, '###order');
       await order.save();
     }
+
     if (body.event === "payment.failed") {
       const order = await Order.findOne({ razorpayOrderId: paymentDetails.order_id });
       order.paymentStatus = "failed";
-      console.log(order, '###order');
       await order.save();
     }
     return new NextResponse("Webhook received", { status: 200 });
