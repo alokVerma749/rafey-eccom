dc61ab3af9de9aa443571c6397257aba08c890e5
alokverma <alokverma749@gmail.com>
2025-01-27 12:03:39 +0530

    fix: build

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,3 @@
-import { Footer } from "../components/Footer";
 import { getSession } from "@/utils/auth";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AdminSidebar } from "../components/Admin/Home/AdminSidebar";
