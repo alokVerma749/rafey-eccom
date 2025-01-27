382979fbf856c1f1b8e19b13cecd122915373093
alokverma <alokverma749@gmail.com>
2025-01-27 12:07:38 +0530

    fix: build

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,12 +1,7 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AdminSidebar } from "../components/Admin/Home/AdminSidebar";
-'use client'
-
-import { usePathname } from "next/navigation";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const isAdmin = pathname.includes('admin');
 
   return (
     <div>
