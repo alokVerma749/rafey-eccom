f3306e9641f68ea925bf1b334a0ff070eec38293
Sachin Chauhan <010sssachin@gmail.com>
2025-02-02 09:55:22 +0530

    loader ui fix and profile structue ready

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader: React.FC = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-30 z-[9999] flex justify-center items-center">
+    <div className="flex justify-center items-center bg-gay-100 bg-opacity-30 z-[9999] h-[100vh]">
       <div className="w-[100px] h-[100px] rounded-full border-[10px] border-gray-300 border-t-orange-500 animate-spin"></div>
     </div>
   );
