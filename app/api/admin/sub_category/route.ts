2b8fd103283d062cd4a804477e9ebe2384bf76e1
alokverma <alokverma749@gmail.com>
2025-02-05 00:16:35 +0530

    fix: add get apis for tags

--- app/api/admin/sub_category/route.ts
+++ app/api/admin/sub_category/route.ts
@@ -4,6 +4,27 @@ import SubCategory from "@/models/sub_category";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 
+export async function GET() {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    await connect_db();
+
+    const subCategories = await SubCategory.find({})
+
+    return NextResponse.json(
+      { success: true, subCategories },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ success: false, message: "Server error" }, { status: 500 });
+  }
+}
+
 export async function POST(req: NextRequest) {
   try {
     const session = await getServerSession(authOptions);
--- app/api/admin/tags/route.ts
+++ app/api/admin/tags/route.ts
@@ -4,6 +4,28 @@ import connect_db from "@/config/db";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 
+export async function GET() {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    await connect_db();
+
+    const tags = await Tag.find({})
+
+    return NextResponse.json(
+      { success: true, tags },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ success: false, message: "Server error" }, { status: 500 });
+  }
+}
+
+
 export async function POST(req: NextRequest) {
   try {
     const session = await getServerSession(authOptions);
