dca0c71005e729056f7912b7f2c43788b518ea13
Sachin Chauhan <010sssachin@gmail.com>
2025-02-05 20:15:04 +0530

    dynamic order details done

--- app/(user)/profile/order-history/[order_id]/page.tsx
+++ app/(user)/profile/order-history/[order_id]/page.tsx
@@ -9,6 +9,9 @@ import { toast } from "@/hooks/use-toast";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import Loader from "@/app/components/Loader";
+import { UserAccount } from "@/models/user_model";
+import { useSession } from "next-auth/react";
+import { getUserAccount } from "@/db_services/user";
 
 type OrderData = {
   _id: string;
@@ -22,7 +25,9 @@ type OrderData = {
 
 const OrderDetails = () => {
   const [orderData, setOrderData] = useState<{ order: OrderData; productDetails: Product[] } | null>(null);
+  const [user, setUser] = useState<UserAccount | null>(null);
   const { order_id } = useParams();
+  const session = useSession();
 
   useEffect(() => {
     const fetchOrder = async () => {
@@ -48,21 +53,28 @@ const OrderDetails = () => {
       }
     };
 
+    const fetchUser = async () => {
+      const userAccount = await getUserAccount(session.data?.user?.email ?? '');
+      const userAccountData = JSON.parse(userAccount);
+      setUser(userAccountData)
+    }
+
     fetchOrder();
-  }, [order_id]);
+    fetchUser();
+  }, [order_id, session.data?.user?.email]);
 
   if (!orderData) {
     return <div><Loader /></div>;
   }
 
   console.log(orderData); // plesae go through the resopnse and try to render it appropriately
-
+  const discountPercentage = orderData.productDetails[0].discount?.percentage || 0;
   return (
-    <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md">
+    <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md">
       <div className="flex justify-between items-center mb-4">
         <div className="flex items-center gap-2">
           <ArrowLeft className="cursor-pointer" />
-          <h2 className="text-lg font-semibold">ORDER NO: 00000029265</h2>
+          <h2 className="text-lg font-semibold"><strong>ORDER NO: </strong>{orderData.order._id} </h2>
         </div>
         <span className="text-sm text-gray-500">Estimated 24/07/2024</span>
       </div>
@@ -70,60 +82,62 @@ const OrderDetails = () => {
       <DeliveryTracker />
 
       <h3 className="text-md font-semibold mb-3">ORDER SUMMARY</h3>
-      <div className="flex items-center border border-gray-300 rounded-lg p-4 mb-4">
-        <div className="mr-4">
+      <div className="flex justify-between items-center border border-gray-300 rounded-lg p-4 mb-4">
+        <div className="flex justify-start items-center gap-x-6">
           <Image
-            src="/ring.png"
+            src={orderData.productDetails[0]?.images?.medium}
             alt="Solitaire Diamond Ring"
             height={80}
             width={80}
             className="object-cover rounded-md"
           />
+          <div className="flex-1 text-sm text-gray-700">
+            <p className="font-semibold">{orderData.productDetails[0].name}</p>
+            <p><strong>Category: </strong>{orderData.productDetails[0].category}</p>
+            <p><strong>Colour: </strong>{orderData.productDetails[0].color}</p>
+            <p><strong>Order Date: </strong>{new Date(orderData.productDetails[0].createdAt).toLocaleDateString()}</p>
+            <p>{orderData.productDetails[0].description}</p>
+
+          </div>
         </div>
-        <div className="flex-1 text-sm text-gray-700">
-          <p className="font-medium">SOLITAIRE DIAMOND RING</p>
-          <p>Size: 24</p>
-          <p>Purity: 14kt</p>
-          <p>Color: Gold</p>
-          <p>Diamond: IJ-SI</p>
-        </div>
-        <span className="font-medium text-gray-800">₹39,999</span>
+        {discountPercentage > 0 && (
+          <div className="flex flex-col items-start justify-start gap-x-4 font-medium">
+            <p className="font-semibold text-black text-sm line-through">${orderData.productDetails[0].price}</p>
+            <p className="text-green-600 text-sm">{discountPercentage} % OFF</p>
+            <p className='text-base font-semibold text-green-600'> ${(orderData.productDetails[0].price - (orderData.productDetails[0].price * discountPercentage) / 100).toFixed(2)}</p>
+          </div>
+        )}
       </div>
 
       <div className="text-sm text-gray-700 mb-4">
         <div className="flex justify-between">
           <span>Subtotal:</span>
-          <span>₹39,999</span>
+          <span></span>
         </div>
         <div className="flex justify-between">
           <span>Delivery:</span>
-          <span>Free</span>
+          <span className="pr-4">Free</span>
         </div>
         <hr className="my-2" />
         <div className="flex justify-between font-semibold">
           <span>TOTAL AMOUNT:</span>
-          <span>₹39,999</span>
+          <p className='text-base font-semibold text-green-600 pr-4'> ${(orderData.productDetails[0].price - (orderData.productDetails[0].price * discountPercentage) / 100).toFixed(2)}</p>
         </div>
       </div>
 
       <div className="text-sm text-gray-700 mb-4">
-        <p>
-          <strong>Order updates sent to:</strong> amsiani@gmail.com | +91 9302847392
-        </p>
+        <p><strong>Order updates sent to:</strong> {user?.email} | {user?.phone}</p>
       </div>
 
       <div className="text-sm text-gray-700 mb-4">
         <p className="font-semibold">Delivering at:</p>
-        <p>
-          52RV-5FR, Golden Apartments, MG Rd, near Kapadia Hospital, Goregaon
-          (W), Mumbai, Maharashtra 400104
-        </p>
-        <p>Phone: +91 96500 39980</p>
+        <p>{user?.address}</p>
+        <p>Phone: {user?.phone}</p>
       </div>
 
       <div className="text-sm text-gray-700 mb-6">
-        <p className="font-semibold">Mode of Payment:</p>
-        <p>Online payment (credit card)</p>
+        <p className="font-semibold">Payment ID:</p>
+        <p>{orderData.order.paymentId}</p>
       </div>
 
       <Button className="w-fit bg-indigo-900 text-white flex items-center gap-2 px-10">
--- app/components/Profile/DeliveryTracker.tsx
+++ app/components/Profile/DeliveryTracker.tsx
@@ -28,7 +28,7 @@ export function DeliveryTracker() {
   }, []);
 
   return (
-    <div className="max-w-xl mx-auto p-6 bg-white rounded-lg shadow-md">
+    <div className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-md mb-4">
       <h2 className="text-lg font-semibold text-gray-800 mb-4 text-center">Delivery Tracker</h2>
       
       <div className="flex items-center justify-between text-sm text-gray-700 mb-4">
