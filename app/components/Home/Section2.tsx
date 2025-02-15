a2169d60806375fd4a0350fc5067d4599e035c78
Sachin Chauhan <010sssachin@gmail.com>
2025-02-15 21:01:51 +0530

    sales falg modified

--- app/components/Home/Section2.tsx
+++ app/components/Home/Section2.tsx
@@ -28,11 +28,13 @@ async function Section2() {
           <Link
             key={index}
             href={`/product/${product._id}`}
-            className="cursor-pointer relative shadow-md rounded text-center transition-transform duration-300 hover:scale-105 w-full xs:w-5/12 sm:w-1/3 md:w-1/3 lg:w-1/4 xl:w-1/5"
+            className="cursor-pointer relative shadow-md rounded text-center transition-transform duration-300 hover:scale-105 w-full xs:w-5/12 sm:w-1/3 md:w-1/3 lg:w-1/4 xl:w-1/5 overflow-hidden"
           >
             {product.onSale && (
-              <div className="absolute top-[-8] right-[-8] bg-red-500 text-white text-xs font-bold px-2 py-1 rounded">
-                On Sale
+              <div className="absolute top-0 left-0 w-full h-full flex justify-center items-start">
+                <p className="bg-yellow-600 text-white px-4 pl-6 py-1 text-xs font-bold rotate-45 absolute top-2 left-2">
+                  On Sale
+                </p>
               </div>
             )}
             <Image
