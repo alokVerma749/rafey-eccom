b614af98b1cc71e9bbe1086ce7a624abc89c1bba
alokverma <alokverma749@gmail.com>
2025-02-21 12:47:08 +0530

    fix: build

--- app/(user)/cart/page.tsx
+++ app/(user)/cart/page.tsx
@@ -15,7 +15,6 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ProcessingAnimation } from "@/app/components/PrcessingAnimation";
 import Loader from "@/app/components/Loader";
-import { useCartData } from '@/hooks/useCartData';
 
 interface orderTypes {
   user: string;
