bd01d31851a2ff9edff6fc5698fea8a2c98b6040
alokverma <alokverma749@gmail.com>
2025-02-27 21:01:28 +0530

    fix: build

--- app/components/Admin/messages/MessageCard.tsx
+++ app/components/Admin/messages/MessageCard.tsx
@@ -1,5 +1,4 @@
-import { MessageSquareOff } from "lucide-react"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Message } from "@/types/message"
 
 export function MessageCard({ msg }: { msg: Message }) {
