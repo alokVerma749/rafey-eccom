2048da679c039d6e881e5dfbaff1f3efab106cbd
Sachin Chauhan <010sssachin@gmail.com>
2025-02-27 19:41:59 +0530

    fixes minor

--- middleware.ts
+++ middleware.ts
@@ -43,7 +43,7 @@ export async function middleware(req: NextRequest) {
     }
 
     // If route is admin protected but user is not an admin, redirect
-    if (isAdminProtectedRoute && userData.user.role !== "user") {
+    if (isAdminProtectedRoute && userData.user.role !== "admin") {
       console.warn("Non-admin user attempted to access admin route.");
       return NextResponse.redirect(new URL("/", req.url));
     }
